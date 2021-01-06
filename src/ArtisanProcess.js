df0a2eee45ffe3907fe17d631a2f0efa37052c34
Obarey <ahmetkanbur@gmail.com>
2021-01-06 17:52:20 +0300

    refactor: implement redis sub version

--- src/ArtisanProcess.js
+++ src/ArtisanProcess.js
@@ -5,11 +5,41 @@ const cp = require('child_process');
  */
 function ArtisanProcess (props)
 {
-    this.props      = props;
-    this.process    = null;
-    this.createdAt  = Date.now() / 1000;
-
-    this.spawnProcess();
+    /**
+     * Process props.
+     * @type {object}
+     */
+    this.props = props;
+    /**
+     * Redis topic for the process.
+     * @type {string}
+     */
+    this.topic = '';
+    /**
+     * Message expected from Artisan process to indicate it's spawned successfully.
+     * @type {string}
+     */
+    this.spawnMessage = 'spawned';
+    /**
+     * Process instance.
+     * @type {null|ChildProcess}
+     */
+    this.process = null;
+    /**
+     * Creation timestamp of the process. ( Unix stamp in seconds )
+     * @type {number}
+     */
+    this.createdAt = Date.now() / 1000;
+    /**
+     * Spawned flag.
+     * @type {boolean}
+     */
+    this.spawned = false;
+    /**
+     * Killed flag.
+     * @type {boolean}
+     */
+    this.killed = false;
 }
 
 /**
@@ -17,14 +47,20 @@ function ArtisanProcess (props)
  *
  * @return void
  */
-ArtisanProcess.prototype.spawnProcess = function()
+ArtisanProcess.prototype.spawn = function()
 {
-    this.process = cp.spawn(
-        'php',
-        [this.props.artisanPath, this.props.artisanCommand, this.props.arguments],
-        { stdio: ['pipe', process.stdout, process.stderr] });
+    this.createdAt = Date.now() / 1000;
+    this.topic     = this.props.tenant + '_' + Date.now();
 
-    this.setUpProcessCallbacks();
+    try {
+        this.process = cp.spawn(
+            'php',
+            [this.props.artisanPath, this.props.artisanCommand, this.props.tenant, this.topic]);
+
+        this.setUpProcessCallbacks();
+    } catch( error ) {
+        console.log('ArtisanProcess spawn error!!!.', {props: this.props});
+    }
 };
 
 /**
@@ -34,32 +70,38 @@ ArtisanProcess.prototype.spawnProcess = function()
  */
 ArtisanProcess.prototype.setUpProcessCallbacks = function()
 {
+    // When Artisan process is spawned, we expect a message
+    // from it to understand it's spawned with no problems.
+    this.process.stdout.on('data', (message) => {
+        if (message.toString() === this.spawnMessage) {
+            this.spawned = true;
+        }
+    });
+
     this.process.on('error', (err) => {
-       console.log('ArtisanProcess spawn error.', { error: err});
+        console.log('ArtisanProcess spawn error.', {tenant: this.props.tenant, error: err});
+        this.killed = true;
     });
 
     this.process.on('close', (code, signal) => {
-        console.log('ArtisanProcess killed.', {code: code, signal: signal});
+        console.log('ArtisanProcess killed.', {tenant: this.props.tenant, code: code, signal: signal});
+        this.killed = true;
     });
 };
 
-/**
- * Pipes data to process.
- *
- * @param data: string
- */
-ArtisanProcess.prototype.pipeData = function(data)
-{
-    this.process.stdin.write(data + '\n');
-};
+
 
 /**
  * Kills process.
  */
 ArtisanProcess.prototype.kill = function()
 {
-    this.process.stdin.end();
-    this.process.kill();
+    try {
+        this.process.stdin.end();
+        this.process.kill();
+    } catch(error) {
+        console.log('ArtisanProcess kill error.', {tenant: this.props.tenant});
+    }
 };
 
 /**
@@ -70,6 +112,26 @@ ArtisanProcess.prototype.kill = function()
 ArtisanProcess.prototype.isExpired = function()
 {
     return (Date.now() / 1000) - this.createdAt > parseInt(this.props.processExpiresIn);
+};
+
+/**
+ * Returns whether process is spawned or not.
+ *
+ * @returns {boolean}
+ */
+ArtisanProcess.prototype.isSpawned = function()
+{
+    return this.spawned;
+};
+
+/**
+ * Returns Redis topic name.
+ *
+ * @returns {string}
+ */
+ArtisanProcess.prototype.getTopicName = function()
+{
+    return this.topic;
 };
 
 module.exports = ArtisanProcess;
--- src/nodes/artisan-process-controller.js
+++ src/nodes/artisan-process-controller.js
@@ -11,88 +11,48 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
 
         /**
-         * @var null|ArtisanProcess
+         * Active Artisan process.
+         * @type {null|ArtisanProcess}
          */
-        this.process             = null;
-        this.notificationPool    = [];
-        this.receivedNotfCounter = 0;
-        this.pipedBatchCounter    = 0;
-        this.batchCounter        = 0;
-        this.batchSize           = 300;
+        this.process = null;
+        /**
+         * New Artisan process used during transition.
+         * @type {null|ArtisanProcess}
+         */
+        this.newProcess = null;
+        /**
+         * Redis topic process uses to publish.
+         * @type {string}
+         */
+        this.activeTopic = '';
+        /**
+         * Process transition check flag for watcher.
+         * @type {boolean}
+         */
+        this.transition = false;
 
         this.artisanConfig  = {
             artisanPath      : config.artisanPath,
             artisanCommand   : config.artisanCommand,
             processExpiresIn : config.expiresIn,
-            arguments        : config.arguments,
+            tenant           : config.tenant,
         };
 
         this.spawnNewProcess();
 
+        /**
+         * Watcher for process checks.
+         * @type {NodeJS.Timeout}
+         */
+        this.watcher = setInterval(() => {
+            this.checkProcess();
+        }, 5000);
+
         // Callback for incoming messages.
-        this.on('input', (msg) => {
-            this.receivedNotfCounter++;
-
-            this.handleInput(JSON.stringify(msg.payload));
-
-            // Output monitoring data.
-            var piped = this.pipedBatchCounter * this.batchSize;
-            this.send({
-                received : this.receivedNotfCounter,
-                piped    : piped,
-                diff     : this.receivedNotfCounter - piped,
-            });
+        this.on('input', () => {
+            this.send({topic: this.activeTopic});
         });
     }
-
-    /**
-     * Handles input message.
-     *
-     * @return void
-     */
-    ArtisanProcessController.prototype.handleInput = function (msg)
-    {
-        this.pushData(msg);
-    };
-
-    /**
-     * Push data to notification pool.
-     *
-     * @param data: string
-     *
-     * @return void
-     */
-    ArtisanProcessController.prototype.pushData = function(data)
-    {
-        this.notificationPool.push(data);
-        this.batchCounter++;
-
-        if (this.batchCounter === this.batchSize) {
-            this.checkProcess();
-
-            this.pipe(this.notificationPool.slice(0));
-
-            this.notificationPool = [];
-            this.batchCounter     = 0;
-            this.pipedBatchCounter++;
-        }
-    };
-
-    /**
-     * Pipes batch to process.
-     *
-     * @param batch: Array<string>
-     */
-    ArtisanProcessController.prototype.pipe = function(batch)
-    {
-        try {
-            for (let k = 0; k < this.batchSize; k++) {
-                this.process.pipeData(batch[k]);
-            }
-        } catch (error) {
-            console.error('A.P.C pipe error.', {tenant: this.tenant.name});
-        }
-    };
 
     /**
      * Checks whether current Artisan process is expired or not.
@@ -101,9 +61,39 @@ module.exports = function (RED) {
      */
     ArtisanProcessController.prototype.checkProcess = function()
     {
-        if (this.process.isExpired()) {
-            this.process.kill();
+        // If transition flag is set; it means previously
+        // watcher attempted to spawn a new process.
+        if (this.transition) {
+            // Here we check if new process spawn attempt is
+            // successful or not.
+            if (this.newProcess.isSpawned()) {
+                // New process is spawned, we move on
+                // to kill old process.
+                try {
+                    console.log('kill old');
+                    this.process.kill();
+                } catch (err) {
+                    console.log(err);
+                    // This try-catch is used to suppress errors
+                    // on initial start where this.process is null.
+                }
+                // Update topic name with the new one.
+                this.activeTopic = this.newProcess.getTopicName();
+                // Replace active process with new one.
+                this.process = this.newProcess;
+                this.newProcess = null;
+                // Clear transition flag.
+                this.transition = false;
+                console.log('APC Transition completed.');
+            }
+            // Do not continue after this.
+            return;
+        }
 
+        // When there is no transition, we check active
+        // process for expiration.
+        if (this.process.isExpired()) {
+            console.log('APC process expired.');
             this.spawnNewProcess();
         }
     };
@@ -115,7 +105,10 @@ module.exports = function (RED) {
      */
     ArtisanProcessController.prototype.spawnNewProcess = function ()
     {
-        this.process = new ArtisanProcess(this.artisanConfig);
+        console.log('APC spawn new process.', {config: this.artisanConfig});
+        this.transition = true;
+        this.newProcess = new ArtisanProcess(this.artisanConfig);
+        this.newProcess.spawn();
     };
 
     RED.nodes.registerType('artisan-process-controller', ArtisanProcessController);
